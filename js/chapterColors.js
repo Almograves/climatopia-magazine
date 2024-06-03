523d20b38109f8b2d76a547562e93b358cf6d24b
Almograves <rageOn110@protonmail.com>
2024-06-03 23:37:10 +0900

    finished cactus intro page

--- js/chapterColors.js
+++ js/chapterColors.js
@@ -5,12 +5,12 @@ sections.forEach((section) => {
     // Get the color of the current chapter
     const color1 = getComputedStyle(section).getPropertyValue('--sectionColor1');
 
-    // Get the corresponding footnote
-    const footnote = document.querySelector(`.footnote[data-section="${section.id}"]`);
+    // Get the corresponding footnotes
+    const footnotes = document.querySelectorAll(`.footnote[data-section="${section.id}"]`);
 
-    // Apply the color to the footnote
-    if (footnote) {
+    // Apply the color to each footnote
+    footnotes.forEach((footnote) => {
         footnote.style.setProperty('--sectionColor1', color1);
-    }
+    });
 
 });
